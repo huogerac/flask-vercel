f6ff469e3d403bc08e0a320b68dd0fe6f6850e2e
Roger Camargo <huogerac@gmail.com>
2021-07-28 20:44:56 -0300

    add the first test

---
+++ tests/api/test_api.py
@@ -0,0 +1,8 @@
+def test_should_get_the_api_version_success_code(client):
+
+    response = client.get(
+        "/api/version",
+        headers={"content-type": "application/json"},
+    )
+
+    assert response.status_code == 200
